2a2712deaf3f2965ddde26a5e5ec69a471d4cef4
Hampus Åström <hampus.thep@gmail.com>
2018-09-16 20:31:22 +0200

    Added simple evaporation/precipitation, and prints in func

--- main.py
+++ main.py
@@ -78,45 +78,86 @@ def flow_water(rock_map, water_map, scale_fac):
     water_map = water_map.clip(min = 0)
     return rock_map, water_map
 
+def evap(rock_map, water_map, scale_fac):
+    # sum_water = sum(sum(water_map))
+    evap = water_map * scale_fac
+    evap_sum = sum(sum(evap))
+    x, y = water_map.shape
+    rain_per = evap_sum / (x * y)
+    water_map = water_map - evap + rain_per
+    return rock_map, water_map
 
+def plot_4(rock, water, X, Y, minc, maxc, i):
+    fig = plt.figure(i)
+    ax = fig.add_subplot(221, projection='3d')
+    ax.plot_surface(X, Y, rock, cmap='jet', edgecolor="none")
+    ax = fig.add_subplot(222, projection='3d')
+    ax.plot_surface(X, Y, water, cmap='jet', edgecolor="none")
+    ax = fig.add_subplot(223, projection='3d')
+    ax.plot_surface(X, Y, rock + water, cmap='jet', edgecolor="none")
+    ax = fig.add_subplot(224, projection='3d')
+    norm = matplotlib.colors.Normalize(minc, maxc)
+    m = plt.cm.ScalarMappable(norm=norm, cmap='terrain')
+    m.set_array([])
+    cmap = m.to_rgba(-water)
+    ax.plot_surface(X, Y, rock, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
+    ax.plot_surface(X, Y, rock + water, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
+    ax.set_zlim(zmin = 0, zmax = 200)
+    plt.show()
 
-# test = np.zeros((4, 4))
-# test_rock = np.zeros((4, 4))
-# test_water = np.zeros((4, 4))
-# test_rock[1, 1] = 1
-# test_rock[1, 2] = 1
-# test_water[1, 1] = 1
-# test_water[3, 3] = 1
-# test = test_rock + test_water
-#
-# print('test:')
-# print(test)
-#
-# up = flow_to_dir(test, 'up', 0.25)
-# print('up:')
-# print(up)
-# down = flow_to_dir(test, 'down', 0.25)
-# print('down:')
-# print(down)
-# left = flow_to_dir(test, 'left', 0.25)
-# print('left:')
-# print(left)
-# right = flow_to_dir(test, 'right', 0.25)
-# print('right:')
-# print(right)
-#
-# print()
-# print()
-# print()
-#
-# test_rock, test_water = flow_water(test_rock, test_water, 0.25)
-#
-# print('test_rock')
-# print(test_rock)
-# print('test_water')
-# print(test_water)
-#
-# exit()
+def plot_1(rock, water, X, Y, minc, maxc, i):
+    fig = plt.figure(i)
+    ax = fig.add_subplot(111, projection='3d')
+    norm = matplotlib.colors.Normalize(minc, maxc)
+    m = plt.cm.ScalarMappable(norm=norm, cmap='terrain')
+    m.set_array([])
+    cmap = m.to_rgba(-water)
+    ax.plot_surface(X, Y, rock, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
+    ax.plot_surface(X, Y, rock + water, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
+    ax.set_zlim(zmin = 0, zmax = 200)
+    plt.show()
+
+def test_func():
+    test = np.zeros((4, 4))
+    test_rock = np.zeros((4, 4))
+    test_water = np.zeros((4, 4))
+    test_rock[1, 1] = 1
+    test_rock[1, 2] = 1
+    test_water[1, 1] = 1
+    test_water[3, 3] = 1
+    test = test_rock + test_water
+
+    print('test:')
+    print(test)
+
+    up = flow_to_dir(test, 'up', 0.25)
+    print('up:')
+    print(up)
+    down = flow_to_dir(test, 'down', 0.25)
+    print('down:')
+    print(down)
+    left = flow_to_dir(test, 'left', 0.25)
+    print('left:')
+    print(left)
+    right = flow_to_dir(test, 'right', 0.25)
+    print('right:')
+    print(right)
+
+    print()
+    print()
+    print()
+
+    # test_rock, test_water = flow_water(test_rock, test_water, 0.25)
+    test_rock, test_water = evap(test_rock, test_water, 0.10)
+
+    print('test_rock')
+    print(test_rock)
+    print('test_water')
+    print(test_water)
+
+    exit()
+
+#test_func()
 
 pi = math.pi
 x_size = 100
@@ -135,6 +176,7 @@ point_list.append(Gaussian_point(0.05, 0.80, 0.20, 0.80, 0.25, 5*pi/16))
 point_list.append(Gaussian_point(0.05, 0.20, 0.25, 0.25, 0.15, 1*pi/32))
 point_list.append(Gaussian_point(0.05, 0.70, 0.15, 0.80, 0.10, 5*pi/32))
 point_list.append(Gaussian_point(0.02, 0.80, 0.10, 0.40, 0.05, 0))
+point_list.append(Gaussian_point(0.02, 0.80, 0.10, 0.60, 0.05, 5*pi/8))
 
 amp_tot = 0
 for point in point_list:
@@ -186,37 +228,21 @@ mean_rock = np.mean(rock)
 
 water = np.zeros((x_size, y_size))
 water += mean_rock/5
+# water[0,0] += x_size*y_size*mean_rock/5
 
-minc = -20
-maxc = 5
+plot_spash = 1
+minc = -5
+maxc = 12
+
 for i in range(10):
-    fig = plt.figure(i)
-    ax = fig.add_subplot(221, projection='3d')
-    ax.plot_surface(X, Y, rock, cmap='jet', edgecolor="none")
-    ax = fig.add_subplot(222, projection='3d')
-    ax.plot_surface(X, Y, water, cmap='jet', edgecolor="none")
-    ax = fig.add_subplot(223, projection='3d')
-    ax.plot_surface(X, Y, rock + water, cmap='jet', edgecolor="none")
-    ax = fig.add_subplot(224, projection='3d')
-    norm = matplotlib.colors.Normalize(minc, maxc)
-    m = plt.cm.ScalarMappable(norm=norm, cmap='terrain')
-    m.set_array([])
-    cmap = m.to_rgba(-water)
-    ax.plot_surface(X, Y, rock, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
-    ax.plot_surface(X, Y, rock + water, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
-    ax.set_zlim(zmin = 0)
-    plt.show()
+    if plot_spash:
+        plot_4(rock, water, X, Y, minc, maxc, i)
 
     for j in range(500):
         rock, water = flow_water(rock, water, 0.10)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-norm = matplotlib.colors.Normalize(minc, maxc)
-m = plt.cm.ScalarMappable(norm=norm, cmap='terrain')
-m.set_array([])
-cmap = m.to_rgba(-water)
-ax.plot_surface(X, Y, rock, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
-ax.plot_surface(X, Y, rock + water, facecolors=cmap, vmin=minc, vmax=maxc, edgecolor="none")
-ax.set_zlim(zmin = 0)
-plt.show()
+for i in range(10):
+    plot_1(rock, water, X, Y, minc, maxc, i)
+    for j in range(500):
+        rock , water = evap(rock, water, 0.0003)
+        rock, water = flow_water(rock, water, 0.10)
